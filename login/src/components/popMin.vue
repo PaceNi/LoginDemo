b509df0e0d95d77cfdc53d386cbac163e8178d47
Pace <993293307@qq.com>
2017-12-06 16:43:10 +0800

    组件

---
+++ login/src/components/popMin.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="popMin animated" :class="{'fadeIn' : popMin&&!popOut , 'hide' : !popMin, 'fadeOut': popOut }">
+    <span class="icon_imp" :class="popClass"></span>
+    <p class="txt">{{ msg }}</p>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+      }
+    },
+    props:[ 'popMin', 'popOut', 'popClass', 'msg'],
+    created () {
+    },
+    components: {},
+    methods: { 
+    },
+    watch:{
+    },
+  }
+</script>
