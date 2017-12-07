1066c47fc13021e1bf71c20fe74b83d65af12a85
Pace <993293307@qq.com>
2017-12-07 21:05:34 +0800

    格式调整

--- test/unit/specs/login.spec.js
+++ test/unit/specs/login.spec.js
@@ -3,7 +3,6 @@ import login from '@/components/login'
 
 describe('login.vue', () => {
 	
-	
 	it('输入‘’toast提示请输入手机号', () => {
 		const Constructor = Vue.extend(login)
 		const vm = new Constructor().$mount()
@@ -42,14 +41,14 @@ describe('login.vue', () => {
 
   it('输入‘18852956187’先返回发送中', () => {
   	const Constructor = Vue.extend(login)
-	const vm = new Constructor().$mount()
-	vm.phoneVal='18852956186'
-	const button = vm.$el.querySelector('.getCode');
-	const clickEvent = new window.Event('click');
-	button.dispatchEvent(clickEvent);
-	vm._watcher.run();
-	const status = vm.$el.querySelector('.status');
-	expect(status.textContent).toEqual('发送中...')
+		const vm = new Constructor().$mount()
+		vm.phoneVal='18852956186'
+		const button = vm.$el.querySelector('.getCode');
+		const clickEvent = new window.Event('click');
+		button.dispatchEvent(clickEvent);
+		vm._watcher.run();
+		const status = vm.$el.querySelector('.status');
+		expect(status.textContent).toEqual('发送中...')
   })
 
 })
